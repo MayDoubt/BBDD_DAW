5ca3efa817e37bc475be457453a65158b4bf0c5e
Fernando Perez <fernando.s256217@cesurformacion.com>
2022-02-17 23:23:52 +0100

    Consultas Arepazo terminadas

--- DML/FERNANDOPEREZNIETO_Arepazo.sql
+++ DML/FERNANDOPEREZNIETO_Arepazo.sql
@@ -34,7 +34,47 @@ inner join Pedidos P on P.IdCliente=C.id
 order by P.Total desc
 
 --Ej6 ¿Cual es el elemento del Menú que más se ha vendido? Ten en cuenta las cantidades.
-Select M.Nombre, SUM(PL.Cantidad) from Menu M
-inner join PedidosLinea PL on PL.IdMenu=M.Id
 
-Select * from clientes
+Select M.Nombre, SUM(PL.Cantidad) as cantidad from Menu M
+inner join PedidosLinea PL on PL.IdMenu=M.Id
+group by M.Nombre
+order by cantidad desc
+
+--Ej7 Saca una factura desglosando el precio con iva y sin iva de cada linea del pedido numero 1.
+      -- Los precios están con IVA y el IVA dependerá del tipo de elemento.
+      -- En otra consulta, sacar el total desglosado, con IVA y sin IVA. Los gastos de envio no tienen IVA.
+--Desglose
+Select  M.Nombre as Producto, PL.Cantidad, cast(PL.PrecioVentaUnitario/((PL.IVA+100.00)/100) as decimal(5,2))as PrecioSinIVA, cast(PL.IVA as varchar) +'%' as IVA , PL.PrecioVentaUnitario, (PL.PrecioVentaUnitario*PL.Cantidad) as PrecioTotalUnidades from Pedidos P
+inner join PedidosLinea PL on PL.IdPedido=P.Id
+inner join Menu M on M.Id=PL.Id
+where P.Id=1
+order by PL.IVA desc
+--Total SinIVA,ConIVA
+Select 'Total' as ' ', SUM(PrecioSinIVA) as TotalSinIVA, SUM(PrecioVentaUnitario) as TotalConIVA  from
+(Select  M.Nombre as Producto, PL.Cantidad, cast(PL.PrecioVentaUnitario/((PL.IVA+100.00)/100) as decimal(5,2))as PrecioSinIVA, PL.PrecioVentaUnitario from Pedidos P
+inner join PedidosLinea PL on PL.IdPedido=P.Id
+inner join Menu M on M.Id=PL.Id
+where P.Id=1) as t1
+
+--Ej8 Busca los elementos del menú  que nunca nadie ha pedido
+
+Select M.Nombre from Menu M
+left join PedidosLinea PL on M.Id=PL.IdMenu
+where PL.IdMenu is null
+order by M.Nombre desc
+
+--Ej9 Calcula cuanto ha ingresado el Arepazo por meses y año. ( Sumando el total del pedido)
+
+Set Language Spanish
+Select DATENAME(month,Fecha_pedido) as months ,cast(SUM(P.Total) as varchar) + '€' as TotalMes from Pedidos P
+group by DATENAME(month,Fecha_pedido), month(Fecha_pedido), year(Fecha_pedido)
+order by year(Fecha_pedido), month(Fecha_pedido) asc
+
+--Ej10 ¿Que cliente ha hecho mas pedidos?
+
+Select Top 1 C.Nombre, C.Apellido, COUNT(P.Id) as numeroPedidos from Pedidos P
+inner join Clientes C on C.id=P.IdCliente
+group by C.Id,C.Nombre, C.Apellido
+order by numeroPedidos desc
+
+--Hay nombres repetidos por la generación aleatoria por eso hay que filtrar por ambas cosas y para asegurarse incluso del Id
